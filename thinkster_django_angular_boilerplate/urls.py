82640a9d135de9881aa8e4780a5833ba4ac16f98
Taylor Allred <tallred@izeni.com>
2014-12-04 16:31:27 -0700

    broken for some reason

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,20 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 
-from thinkster_django_angular_boilerplate.views import IndexView
+from thinkster_django_angular_boilerplate.views import IndexView, TemplateView
+
+from rest_framework_nested import routers
+
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
 
     url('^.*$', IndexView.as_view(), name='index'),
+
+    url(r'^api/v1/', include(router.urls)),
+
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 )
